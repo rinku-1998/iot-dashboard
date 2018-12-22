c1815b68e3a6719f8bee3653e85525b496b04c5f
www563214789 <www563214789@gmail.com>
2018-12-23 01:03:08 +0800

    Add import dotenv in config

--- config.py
+++ config.py
@@ -1,13 +1,16 @@
 import os
+from dotenv import load_dotenv
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you will never guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    #MQTT_BROKER_URL = '192.168.198.132'
-    MQTT_BROKER_URL = '137.116.170.68'
+    MQTT_BROKER_URL = '192.168.198.132'
+    #MQTT_BROKER_URL = '137.116.170.68'
     MQTT_BROKER_PORT = 1883
     MQTT_REFRESH_TIME = 1.0  # refresh time in seconds
     MQTT_USERNAME = ''  # set the username here if you need authentication for the broker
